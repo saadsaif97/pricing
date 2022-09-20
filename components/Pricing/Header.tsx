e992c1be5a399c9faa990803ed0024a6a20a6553
saad <saadgfx97@gmail.com>
2022-09-20 15:35:39 +0500

    fixed the header color

--- components/Pricing/Header.tsx
+++ components/Pricing/Header.tsx
@@ -4,7 +4,7 @@ import MaxWidth from '../MaxWidth'
 
 export default function Header() {
   return (
-    <Box background="brandDark.900" color="gray.50" textAlign={{ base: "left", md: "center" }} pt={{ base: 14, md: 24 }} pb={48}>
+    <Box background="brandDark.solid" color="gray.50" textAlign={{ base: "left", md: "center" }} pt={{ base: 14, md: 24 }} pb={48}>
      <MaxWidth>
        <Heading fontSize={{ base: "3xl", md: "5xl" }} mb="4">Simple pricing for your business</Heading>
        <Text fontSize="2xl">Plans that are carefully crafted to suit your business.</Text>
